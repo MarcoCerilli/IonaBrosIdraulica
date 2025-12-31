ddbc0073675f2c3f0b40661270c6c8b79eae48f3
marco cerilli <cerillimarco15@gmail.com>
2025-12-31 08:45:40 +0000

    ok allora cambia il numero di whatsapp con questo in tutto il sito +39 3

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 // ===> INSERISCI QUI IL TUO NUMERO WHATSAPP <===
-// Sostituisci "391234567890" con il tuo numero, completo di prefisso internazionale (es. 39 per l'Italia).
+// Sostituisci "393393433568" con il tuo numero, completo di prefisso internazionale (es. 39 per l'Italia).
 // NON usare il simbolo '+' o spazi.
 export const whatsappNumber = '393393433568';
